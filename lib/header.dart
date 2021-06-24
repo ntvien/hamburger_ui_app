4f0d2bcb91b2c742d2718f129ddb7c3877e9d404
Fiction_NTV <thevien898@gmail.com>
2021-06-24 15:52:52 +0700

    fix size of header

--- lib/header.dart
+++ lib/header.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Header extends StatefulWidget {
@@ -12,36 +13,103 @@ class _HeaderState extends State<Header> {
     return SliverList(
       delegate: SliverChildListDelegate(
         [
-          Column(
+          Stack(
             children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                height: size.height / 5,
-                decoration: BoxDecoration(
-                  color: Colors.teal,
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(45),
-                  ),
-                  boxShadow: [
-                    BoxShadow(blurRadius: 2),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.white70,
-                          radius: 35,
-                          child: CircleAvatar(
-                            //backgroundColor: AssetImage(),
-                            radius: 30,
-                          ),
-                        ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    height: size.height / 5,
+                    decoration: BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(45),
+                      ),
+                      boxShadow: [
+                        BoxShadow(blurRadius: 2),
                       ],
-                    )
-                  ],
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.white70,
+                              radius: 35,
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage("images/dog.png"),
+                                radius: 30,
+                              ),
+                            ),
+                            SizedBox(width: 5),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Vien Nguyen",
+                                    style: TextStyle(
+                                      color: Colors.black54,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 23,
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(4),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.black54,
+                                    ),
+                                    child: Text(
+                                      "GOLD MEMBER",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.yellow,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              "40 \$ USA",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: 50,
+                  width: size.width,
+                  child: Card(
+                    elevation: 3,
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: "What does your belly you want to eat?",
+                        suffixIcon: Icon(Icons.search),
+                        contentPadding: EdgeInsets.only(left: 20),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
