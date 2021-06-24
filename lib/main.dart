b0a2d9c56154f80caac1c5d4b463e98073ce3161
Fiction_NTV <thevien898@gmail.com>
2021-06-24 15:53:07 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_hamberger/hambuger_screen.dart';
 import 'header.dart';
 
 void main() {
@@ -9,43 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: AppBarTheme(color: Colors.teal, centerTitle: true),
       ),
-      home: Hamberger(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class Hamberger extends StatefulWidget {
-  @override
-  _HambergerState createState() => _HambergerState();
-}
-
-class _HambergerState extends State<Hamberger> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            title: Text("Deviver Me"),
-            leading: IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {},
-            ),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
-              )
-            ],
-          ),
-          Header(),
-        ],
-      ),
+      home: HamburgerScreen(),
     );
   }
 }
