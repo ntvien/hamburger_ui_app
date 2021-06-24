f8e10b1d40af84670f21efbe9d9135eb6a57775d
Fiction_NTV <thevien898@gmail.com>
2021-06-24 18:35:36 +0700

    add hamburger list screen

---
+++ lib/screen/hamburger_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HamburgerDetail extends StatefulWidget {
+  static const tag = "Burger Page";
+  @override
+  _HamburgerDetailState createState() => _HamburgerDetailState();
+}
+
+class _HamburgerDetailState extends State<HamburgerDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
