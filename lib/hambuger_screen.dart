79ed1197c97f1f536651dc145dc5c633dc760005
Fiction_NTV <thevien898@gmail.com>
2021-06-24 17:29:24 +0700

    add ui bottom app bar and floating action button

--- lib/hambuger_screen.dart
+++ lib/hambuger_screen.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_hamberger/categories.dart';
 import 'package:flutter_app_hamberger/header.dart';
 
 class HamburgerScreen extends StatefulWidget {
-
   @override
   _HamburgerScreenState createState() => _HamburgerScreenState();
 }
@@ -28,14 +29,47 @@ class _HamburgerScreenState extends State<HamburgerScreen> {
             ],
           ),
           Header(),
+          Categories(),
           SliverList(
-            delegate: SliverChildListDelegate(
-                [
-                  Text("Hamburger", style: TextStyle(fontSize: 20),),
-                ]
-            ),
-          )
+            delegate: SliverChildListDelegate([
+              Text(
+                "Hamburger",
+                style: TextStyle(fontSize: 200),
+              ),
+            ]),
+          ),
         ],
+      ),
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.home),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(45)),
+        child: Container(
+          color: Colors.black38,
+          child: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            child: Row(
+              children: [
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.add_alert, color: Colors.white),
+                  onPressed: () {},
+                ),
+                Spacer(),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.turned_in, color: Colors.white),
+                  onPressed: () {},
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
