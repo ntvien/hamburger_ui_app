60db2997f0f10ac37d5c4920c9686813717365c9
Fiction_NTV <thevien898@gmail.com>
2021-06-24 15:01:21 +0700

    create project first
    create project first

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'header.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.teal, centerTitle: true),
+      ),
+      home: Hamberger(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Hamberger extends StatefulWidget {
+  @override
+  _HambergerState createState() => _HambergerState();
+}
+
+class _HambergerState extends State<Hamberger> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            title: Text("Deviver Me"),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+              )
+            ],
+          ),
+          Header(),
+        ],
+      ),
+    );
+  }
+}
